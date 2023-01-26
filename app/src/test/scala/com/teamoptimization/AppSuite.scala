e528abf50a7c5de33e6ab8e1a8d559f0143e180b
Tom Peck <tom.peck.consultant@springernature.co>
2023-01-26 09:56:55 +0000

    Add instructions

--- app/src/test/scala/com/teamoptimization/AppSuite.scala
+++ app/src/test/scala/com/teamoptimization/AppSuite.scala
@@ -4,6 +4,17 @@ import org.junit.runner.RunWith
 import org.scalatest.funsuite.AnyFunSuite
 import org.scalatestplus.junit.JUnitRunner
 
+/*
+This is a list of items such as recently dialled numbers on your phone, or
+recently opened files in an application on your computer.
+• The list should be empty when initialised.
+• We should be able to add things to the list.
+• We should be able to retrieve items from the list.
+• The most recent item should be first in the list.
+• Items in the list are unique, so duplicate insertions should be moved
+rather than added.
+ */
+
 @RunWith(classOf[JUnitRunner])
 class AppSuite extends AnyFunSuite {
   test("App has a greeting") {
