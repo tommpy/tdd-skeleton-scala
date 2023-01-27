be905c80488cfc553a0d49b5567a0f93c9178d46
James <james.byrne@springernature.com>
2023-01-27 15:32:16 +0100

    A change

--- app/src/main/scala/com/teamoptimization/RecentlyUsedList.scala
+++ app/src/main/scala/com/teamoptimization/RecentlyUsedList.scala
@@ -3,11 +3,12 @@ package com.teamoptimization
 class RecentlyUsedList {
   private var element: String = ""
 
-  var length = 2
+  var length = 0
 
   def getLastUsed: String = element
 
   def add(str: String): Unit = {
+    length = length+1
     element = str
   }
   def isEmpty: Boolean = element.isEmpty
