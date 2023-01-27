0c970fcc0b2127d54eddffd1d39723989beb89c7
qlibin <alexander.libin.consultant@springernature.com>
2023-01-27 14:15:30 +0100

    mob next [ci-skip] [ci skip] [skip ci]
    lastFile:app/src/main/scala/com/teamoptimization/RecentlyUsedList.scala

--- app/src/main/scala/com/teamoptimization/RecentlyUsedList.scala
+++ app/src/main/scala/com/teamoptimization/RecentlyUsedList.scala
@@ -1,10 +1,9 @@
 package com.teamoptimization
 
 class RecentlyUsedList {
+  private var somethingHasBeenAdded = false
   def add(str: String): Unit = {
-
+    somethingHasBeenAdded = true
   }
-
-  val isEmpty: Boolean = true
-
+  def isEmpty: Boolean = !somethingHasBeenAdded
 }
